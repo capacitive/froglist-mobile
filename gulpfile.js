a665e934f7944a9671a930a2288136d29a67d39a
Mark Cafazzo <mark.cafazzo@capacitive.ca>
2016-02-15 16:39:46 -0500

    sass and gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -5,29 +5,55 @@ var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 var sh = require('shelljs');
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass: ['./scss/**/*.scss'],
+  sass_src: ['./www/sass'],
+  sass_dest: ['./www/css']
 };
 
 gulp.task('default', ['sass']);
 
 gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
+  browserSync({
+    files: ['www/*', 'www/**/*'],
+    server: {baseDir:['www'],
+    index: 'index.html'
+  }
+  });
+
+  gulp.src(['./scss/ionic.app.scss', './www/sass/*.scss'])
     .pipe(sass())
     .on('error', sass.logError)
-    .pipe(gulp.dest('./www/css/'))
+    .pipe(gulp.dest('./www/css'))
     .pipe(minifyCss({
       keepSpecialComments: 0
     }))
     .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
     .on('end', done);
 });
 
 gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
+});
+
+gulp.task('sass-styles', function(){
+  gulp.src(paths.sass_src)
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest(paths.sass_dest))
+  .pipe(reload({stream:true}));
+});
+gulp.task('sasswatch', function(){
+  browserSync({
+    files: ['www/*'],
+    server: {baseDir:['www'],
+    index: 'index.html'
+  }
+  });
+  gulp.watch(paths.sass_src, ['sass-styles'])
 });
 
 gulp.task('install', ['git-check'], function() {
